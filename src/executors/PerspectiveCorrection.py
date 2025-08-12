25a2f839fafca78259ae0058b15d276774de616a
melked <ce.melikedemirtas@gmail.com>
2025-08-12 10:01:13 +0300

    fix: import fix

--- src/executors/PerspectiveCorrection.py
+++ src/executors/PerspectiveCorrection.py
@@ -3,15 +3,18 @@ import sys
 import cv2
 import numpy as np
 from typing import List
+
 from PIL import Image as PILImage
+
+
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../../../../"))
 
 from sdks.novavision.src.media.image import Image
 from sdks.novavision.src.base.component import Component
 from sdks.novavision.src.helper.executor import Executor
-from components.PerspectiveTransformation.src.utils.response import build_response
-from components.PerspectiveTransformation.src.models.PackageModel import PackageModel
+from components.PerspectiveCorrection.src.utils.response import build_response
+from components.PerspectiveCorrection.src.models.PackageModel import PackageModel
 
 
 class Params:

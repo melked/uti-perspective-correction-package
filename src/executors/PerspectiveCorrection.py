7897e30fc4f3df73d2215dea9568f7aed3e1fb3b
melked <ce.melikedemirtas@gmail.com>
2025-08-21 10:22:54 +0300

    feat: i changed some part

--- src/executors/PerspectiveCorrection.py
+++ src/executors/PerspectiveCorrection.py
@@ -2,18 +2,18 @@ import os
 import sys
 import cv2
 import numpy as np
-from typing import Optional, List, Dict
+from typing import Optional, List, Dict,Tuple
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../../../../"))
 
 from sdks.novavision.src.media.image import Image
 from sdks.novavision.src.base.component import Component
 from sdks.novavision.src.helper.executor import Executor
-from components.PerspectiveTransformation.src.utils.response import build_response
-from components.PerspectiveTransformation.src.models.PackageModel import PackageModel
+from components.PerspectiveCorrection.src.utils.response import build_response
+from components.PerspectiveCorrection.src.models.PackageModel import PackageModel
 
 
-class PerspectiveTransformation(Component):
+class PerspectiveCorrection(Component):
     def __init__(self, request, bootstrap):
         super().__init__(request, bootstrap)
         self.context = {}
